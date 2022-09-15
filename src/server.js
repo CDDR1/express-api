676e2baa44c334cd7784152ddd17594ab5008244
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-09-15 14:57:26 -0400

    Created get all tasks endpoint

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 import express from "express";
 const app = express();
 const PORT = 3000;
+import { getTasks } from "./tasks.js";
 
 // Routes
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("/tasks", (req, res) => {
+  return res.status(200).send({
+    succes: "true",
+    message: "tasks",
+    tasks: getTasks()
+  });
 });
 
 // Initialize server
