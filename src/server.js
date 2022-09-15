e9dfa6ff3b7ab9524fb2f048a84c00c0517272f5
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-09-15 14:34:11 -0400

    Initializing server

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const PORT = 3000;
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// Initialize server
+app.listen(PORT, () => console.log(`Server initialized on port ${PORT}`));
